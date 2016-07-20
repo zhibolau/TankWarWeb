4ee9b3337555387150655f9039432199e79d0cbc
Zhibo Liu <zhibolau@gmail.com>
2016-07-20 12:11:38 -0700

    save client info forudp future use

--- TankWarWeb1.9.2/src/NetClient.java
+++ TankWarWeb1.9.2/src/NetClient.java
@@ -1,3 +1,6 @@
+import sun.nio.ch.Net;
+
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -6,17 +9,43 @@ import java.net.UnknownHostException;
  * Created by zhiboliu on 16-7-20.
  */
 public class NetClient {
-    //public static final
+
+    private static int UDP_PORT_START = 2223;
+    private int udpPort;
+
+    public NetClient(){
+        udpPort = UDP_PORT_START++;
+    }
+
+    //even though u define ur udp port, server does not know
+    //so i have to send it to server
+
 
 
     public void connect(String IP, int port){
+        Socket s = null;
         try {
-            Socket s = new Socket(IP, port);
+            s = new Socket(IP, port);
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeInt(udpPort);
+            //s.close();//after doing sending data, it should be closed
+            //but if sth is wrong with dos.writeInt(udpPort);  u cannot close s, so put it in finally block
 System.out.println("Tank client Connects to server!");
         } catch (UnknownHostException e){
             e.printStackTrace();
         } catch(IOException e) {
             e.printStackTrace();
+        }finally {
+            if(s != null) {
+                try {
+                    s.close();
+                    s = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+
     }
 }
--- TankWarWeb1.9.2/src/TankServer.java
+++ TankWarWeb1.9.2/src/TankServer.java
@@ -1,6 +1,9 @@
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhiboliu on 16-7-20.
@@ -9,15 +12,43 @@ public class TankServer {
 
     public static final int TCP_PORT = 8888;
 
-    public static void main(String[] args){
+    List<Client> clients = new ArrayList<Client>();
+
+    /**
+     * start 方法为动态方法 ，已经有一个this的对象了 所以可以解决直接new client 未先new tankServer的问题
+     */
+    public void start(){
         try {
             ServerSocket ss = new ServerSocket(TCP_PORT);
             while (true){
                 Socket s = ss.accept();
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                String IP = s.getInetAddress().getHostAddress();
+                int udpPort = dis.readInt();
+                Client c = new Client(IP, udpPort); // u cannot new client which is within tankserver without newing tankServer first
+                // use start method to solve this
+                clients.add(c);
+                s.close();
 System.out.println("a client is connected! Address: " + s.getInetAddress() + ":" + s.getPort());
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        new TankServer().start();
+    }
+
+    private class Client{
+        //save some info for udp use in future
+        String IP;
+        // I have to know udp port to send data to client udp
+        int udpPort;
+
+        public Client(String IP, int udpPort){
+            this.IP = IP;
+            this.udpPort = udpPort;
         }
     }
 }
